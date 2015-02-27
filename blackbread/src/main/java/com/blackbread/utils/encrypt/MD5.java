d42cea7ca473d004a1c74a02bcde4e23b7983321
chengxiang.li <chengxiang.li@LICHENGXIANG.autonavi.com>
2015-02-27 18:16:46 +0800

    增加签名功能

--- blackbread/src/main/java/com/blackbread/utils/encrypt/MD5.java
+++ blackbread/src/main/java/com/blackbread/utils/encrypt/MD5.java
@@ -1,6 +1,10 @@
 package com.blackbread.utils.encrypt;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.SignatureException;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class MD5 {
     
@@ -57,4 +61,58 @@ public class MD5 {
         int d2 = n % 16;   
         return hexDigits[d1] + hexDigits[d2];   
      }   
+    
+    /**
+     * 签名字符串
+     * @param text 需要签名的字符串
+     * @param key 密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static String sign(String text, String key, String input_charset) {
+    	text = text + key;
+        return DigestUtils.md5Hex(getContentBytes(text, input_charset));
+    }
+    
+    /**
+     * 签名字符串
+     * @param text 需要签名的字符串
+     * @param sign 签名结果
+     * @param key 密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static boolean verify(String text, String sign, String key, String input_charset) {
+    	text = text + key;
+    	String mysign = DigestUtils.md5Hex(getContentBytes(text, input_charset));
+    	if(mysign.equals(sign)) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+
+    /**
+     * @param content
+     * @param charset
+     * @return
+     * @throws SignatureException
+     * @throws UnsupportedEncodingException 
+     */
+    private static byte[] getContentBytes(String content, String charset) {
+        if (charset == null || "".equals(charset)) {
+            return content.getBytes();
+        }
+        try {
+            return content.getBytes(charset);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("MD5签名过程中出现错误,指定的编码集不对,您目前指定的编码集是:" + charset);
+        }
+    }
+    public static void main(String[] args) {
+		String ssss=MD5.sign("abc",  "1233444", "UTF-8");
+		System.out.println(ssss);
+		System.out.println(MD5.verify("abc", ssss, "1233444", "UTF-8"));
+	}
 }
